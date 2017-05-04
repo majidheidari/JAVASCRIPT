7662233708bc67c824249f1a87ccaf1b308ee456
LENOVO-THINKPAD\Branislav Bily <branislav.bily@gmail.com>
2017-05-04 22:48:47 +0200

    no message

--- TetrisMP/client/main.js
+++ TetrisMP/client/main.js
@@ -4,7 +4,7 @@ tetrisLocal.element.classList.add('local');
 tetrisLocal.run();
 
 const connectionManager = new ConnectionManager(tetrisManager);
-connectionManager.connect();
+connectionManager.connect(); //TODO: do zatvoriek pripojenie na server
 
 const keyListener = (event) => {
     [
