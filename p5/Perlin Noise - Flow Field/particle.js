523518d2f1c2fc9e21b09304caf1496db1121940
LENOVO-THINKPAD\Branislav Bily <branislav.bily@gmail.com>
2017-07-13 23:08:31 +0200

    Beautiful Perlin Noise I cry

---
+++ p5/Perlin Noise - Flow Field/particle.js
@@ -0,0 +1,70 @@
+
+
+function Particle() {
+  this.pos = createVector(random(width - scl), random(height - scl));
+  this.vel = createVector(0, 0);
+  this.acc = createVector(0, 0);
+  this.maxspeed = 1;
+  this.weight = 0;
+
+
+
+  this.prevPos = this.pos.copy();
+
+  this.update = function() {
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxspeed);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+
+  this.follow = function(vectors) {
+    var x = floor(this.pos.x / scl);
+    var y = floor(this.pos.y / scl);
+    var index = x + y * cols;
+    var force = vectors[index];
+    this.applyForce(force);
+  }
+
+  this.applyForce = function(force) {
+    this.acc.add(force);
+  }
+
+  this.show = function() {
+  	colors();
+    //this.hue = noise(this.xoff) * 360;
+    stroke(colour, 255, 255);
+    strokeWeight(this.weight);
+    if (this.weight < 0.1) {
+		this.weight += 0.001;
+    }
+    line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+    //point(this.pos.x, this.pos.y);
+    this.updatePrev();
+  }
+
+  this.updatePrev = function() {
+    this.prevPos.x = this.pos.x;
+    this.prevPos.y = this.pos.y;
+  }
+
+  this.edges = function() {
+    if (this.pos.x > width) {
+      this.pos.x = 0;
+      this.updatePrev();
+    }
+    if (this.pos.x < 0) {
+      this.pos.x = width;
+      this.updatePrev();
+    }
+    if (this.pos.y < 0) {
+      this.pos.y = height;
+      this.updatePrev();
+    }
+    if (this.pos.y > height) {
+      this.pos.y = 0;
+      this.updatePrev();
+    }
+  }
+
+}
---
+++ p5/Perlin Noise - Flow Field/sketch.js
@@ -0,0 +1,82 @@
+var inc = 0.1;
+var scl = 20;
+var cols, rows, slider;
+
+var fr;
+
+var adjust = 0;
+var xoff1 = 0;
+var colour = 0;
+ 
+var zoff = 0;
+var particles = [];
+
+var flowfield;
+
+function setup() {
+  //slider = createSlider(5, 40, 10);
+  frameRate(30);
+  colorMode(HSB);
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  createCanvas (width, height);
+  background(0, 0, 0);
+  cols = floor(width / scl);
+  rows = floor(height / scl);
+  fr = createP('');
+
+  flowfield = new Array(cols * rows)
+
+  for (var i = 0; i < 2048; i++){
+    particles[i] = new Particle();
+  }
+
+}
+function draw() {
+  background(0, 0.01);
+  //scl = slider.value();
+  cols = floor(width / scl);
+  rows = floor(height / scl);
+  var yoff = 0;
+  for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+    for (var x = 0; x < cols; x++) {
+      var index = x + y * cols;
+      var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
+      var v = p5.Vector.fromAngle(angle);
+      v.setMag(1.5);
+      flowfield[index] = v;
+      xoff += inc;
+      //push();
+      //translate(x * scl, y * scl);
+      //rotate(v.heading());
+      //line(0, 0, scl, 0);
+      //pop();
+    }
+    yoff += inc;
+    zoff += 0.002;
+  }
+  var l = particles.length;
+  for (var i = 0; i < l; i++) {
+    particles[i].follow(flowfield);
+    particles[i].update();
+    particles[i].edges();
+    particles[i].show();
+  }
+
+  fr.html(floor(frameRate()));
+
+}
+
+function colors() {
+  colour = noise(xoff1) * 360 / 10000 + adjust;
+  xoff1 += 0.01;
+  if (random() < 0.0001) {
+    adjust += 5;
+  }
+
+  if (colour > 360) {
+    colour = 0;
+    adjust = 0;
+  }
+}
